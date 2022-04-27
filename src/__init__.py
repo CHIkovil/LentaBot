b6ce2035f19ba62b12323550f36986d538a8454d
CHIkovil <chikovil3488@gmail.com>
2022-04-27 18:51:18 +0300

    added initial states

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,14 @@
 import logging
 import conf
+import time
 from emoji import emojize
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.utils.callback_data import CallbackData
+from aiogram.contrib.fsm_storage.mongo import MongoStorage
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 
 logging.basicConfig(level=logging.ERROR)
 LOGGER = logging.getLogger(conf.LOGGER_NAME)
--- src/bot.py
+++ src/bot.py
@@ -1,9 +1,14 @@
 from src import *
 
 
+class StartQuestion(StatesGroup):
+    enter_new_bot_name = State()
+    enter_channels = State()
+
+
 class LentaBot(object):
     _BOT = Bot(token=conf.API_TOKEN)
-    _DP = Dispatcher(_BOT)
+    _DP = Dispatcher(_BOT, storage=MemoryStorage())
 
     def __init__(self):
         pass
@@ -20,15 +25,32 @@ class LentaBot(object):
         """
         This handler will be called when user sends `/start`.
         """
-        await message.reply(emojize("Тута:dodo:"))
+        await message.answer(emojize("Тута:eyes:"))
+        await message.answer(emojize("Псс псс придумай мне кличку!"))
+        await StartQuestion.enter_new_bot_name.set()
+
+    @staticmethod
+    @_DP.message_handler(state=StartQuestion.enter_new_bot_name)
+    async def enter_new_bot_name(message: types.Message):
+        print(message.text)
+        await message.answer(emojize("Учти, отныне я буду прислушиваться только к ней:face_with_tongue:"))
+        await message.answer(emojize("..."))
+        await message.answer(emojize("Ну и наконец перечисли каналы из которых мы сформируем твою ЛИЧНУЮ ленту!"))
+        await message.answer(emojize("Через запятую конечно же."))
+        await StartQuestion.enter_channels.set()
+
+    @staticmethod
+    @_DP.message_handler(state=StartQuestion.enter_channels)
+    async def enter_new_bot_name(message: types.Message, state: FSMContext):
+        print(message.text)
+        await state.finish()
 
     @staticmethod
     @_DP.message_handler()
     async def echo(message: types.Message):
-        """
-        This handler simple resend input message.
-        """
-        await message.answer(message.text)
+        await message.answer("Тах тах не флуди...")
+        await message.answer(emojize("Мы же занимаемся серьезными вопросами:oncoming_fist:"))
+        await message.answer("Не отвлекай")
 
 
 if __name__ == '__main__':
