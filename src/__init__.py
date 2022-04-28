1562db9a792990f740f356156285d8e136ecab6f
CHIkovil <chikovil3488@gmail.com>
2022-04-28 13:28:41 +0300

    debugged save to mongodb and end state

--- src/__init__.py
+++ src/__init__.py
@@ -2,11 +2,10 @@ import logging
 import conf
 import time
 from emoji import emojize
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, executor, types, filters
 from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.utils.callback_data import CallbackData
 from aiogram.contrib.fsm_storage.mongo import MongoStorage
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 
--- src/bot.py
+++ src/bot.py
@@ -2,13 +2,14 @@ from src import *
 
 
 class StartQuestion(StatesGroup):
-    enter_new_bot_name = State()
+    enter_new_bot_nickname = State()
     enter_channels = State()
 
 
 class LentaBot(object):
     _BOT = Bot(token=conf.API_TOKEN)
-    _DP = Dispatcher(_BOT, storage=MemoryStorage())
+    _STORAGE = MongoStorage()
+    _DP = Dispatcher(_BOT, storage=_STORAGE)
 
     def __init__(self):
         pass
@@ -20,19 +21,24 @@ class LentaBot(object):
         self._DP.stop_polling()
 
     @staticmethod
-    @_DP.message_handler(commands=['start'])
-    async def send_welcome(message: types.Message):
+    @_DP.message_handler(filters.CommandStart(), )
+    async def send_welcome(message: types.Message, state: FSMContext):
         """
         This handler will be called when user sends `/start`.
         """
-        await message.answer(emojize("Тута:eyes:"))
-        await message.answer(emojize("Псс псс придумай мне кличку!"))
-        await StartQuestion.enter_new_bot_name.set()
+        data = await state.get_data()
+        if not data:
+            await message.answer(emojize("Тута:eyes:"))
+            await message.answer(emojize("Псс придумай мне кличку!"))
+            await StartQuestion.enter_new_bot_nickname.set()
+        else:
+            await message.answer(emojize(f"Напомню, обращайся ко мне как {data['bot_nickname']}:smirking_face:"))
 
     @staticmethod
-    @_DP.message_handler(state=StartQuestion.enter_new_bot_name)
-    async def enter_new_bot_name(message: types.Message):
-        print(message.text)
+    @_DP.message_handler(state=StartQuestion.enter_new_bot_nickname)
+    async def enter_new_bot_name(message: types.Message, state: FSMContext):
+        async with state.proxy() as proxy:
+            proxy['bot_nickname'] = message.text
         await message.answer(emojize("Учти, отныне я буду прислушиваться только к ней:face_with_tongue:"))
         await message.answer(emojize("..."))
         await message.answer(emojize("Ну и наконец перечисли каналы из которых мы сформируем твою ЛИЧНУЮ ленту!"))
@@ -42,15 +48,16 @@ class LentaBot(object):
     @staticmethod
     @_DP.message_handler(state=StartQuestion.enter_channels)
     async def enter_new_bot_name(message: types.Message, state: FSMContext):
-        print(message.text)
-        await state.finish()
+        async with state.proxy() as proxy:
+            proxy['channels'] = message.text.split(',')
+        await message.answer(emojize("Запомнил:vulcan_salute:"))
+        await state.reset_state(with_data=False)
 
     @staticmethod
     @_DP.message_handler()
     async def echo(message: types.Message):
         await message.answer("Тах тах не флуди...")
         await message.answer(emojize("Мы же занимаемся серьезными вопросами:oncoming_fist:"))
-        await message.answer("Не отвлекай")
 
 
 if __name__ == '__main__':
